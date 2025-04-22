953f6172cd2e9cfe1c108a942ea3f18160923990
Jb Carreon <jbcarreon212@gmail.com>
2025-04-22 17:30:18 +0800

    playing update

--- public/save-dl.sh
+++ public/save-dl.sh
@@ -1,8 +1,9 @@
 yt-dlp -x -f ba \
     --embed-metadata \
     --embed-thumbnail \
+    --audio-format mp3 \
     --convert-thumbnails jpg \
-    --exec-before-download "ffmpeg -i %(thumbnails.-1.filepath)q -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\" _%(thumbnails.-1.filepath)q" \
+    --exec-before-download "ffmpeg -i %(thumbnails.-1.filepath)q -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\" art-%(thumbnails.-1.filepath)q" \
     --exec-before-download "rm %(thumbnails.-1.filepath)q" \
-    --exec-before-download "cp _%(thumbnails.-1.filepath)q %(thumbnails.-1.filepath)q" \
-   $URL
+    --exec-before-download "cp art-%(thumbnails.-1.filepath)q %(thumbnails.-1.filepath)q" \
+    https://music.youtube.com/playlist?list=PLJiNr7HpTYe-PfGB96hQVu504ShqJHQsY
