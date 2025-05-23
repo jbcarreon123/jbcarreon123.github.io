ea0ed4d44166bba7ed856e482efcb67ea26f77c4
Jb Carreon <jbcarreon212@gmail.com>
2025-05-23 14:02:45 +0800

    packages list

--- src/assets/previewTab.ts
+++ src/assets/previewTab.ts
@@ -4,8 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
         button.addEventListener('click', () => {
             let layout = document.querySelector(`.tabLayout#${button.dataset.tabLayout}`) as HTMLDivElement;
             if (!layout) return;
-            document.querySelectorAll('.tabLayout.active').forEach(el => (el as HTMLDivElement).style.display = 'none');
+            document.querySelectorAll('.tabLayout.active').forEach(el => {
+                (el as HTMLDivElement).style.display = 'none';
+                (el as HTMLDivElement).classList.remove('active');
+            });
+            document.querySelectorAll('.tabLayoutButton').forEach(el => {
+                (el as HTMLButtonElement).classList.remove('active');
+            });
             layout.style.display = 'block';
+            layout.classList.add('active');
+            btn.classList.add('active');
         })
     })
 })
