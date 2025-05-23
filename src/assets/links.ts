20e949ab9cbc7bd3b8b17c4e08a8eb39c6cab31e
Jb Carreon <jbcarreon212@gmail.com>
2025-05-23 22:46:26 +0800

    change index to homepage

---
+++ src/assets/links.ts
@@ -0,0 +1,157 @@
+export type LinkObj = {
+    name: string,
+    links: {
+        name: string,
+        path: string,
+        redirect: boolean,
+        icon: string,
+    }[]
+}[]
+
+export const LinkObjs: LinkObj = [
+    {
+        name: "whereabouts",
+        links: [
+            {
+                name: "About JB",
+                path: "/about/",
+                redirect: false,
+                icon: "person"
+            },
+            {
+                name: "Projects",
+                path: "/projects/",
+                redirect: false,
+                icon: "terminal"
+            },
+            {
+                name: "RIIAtW",
+                path: "/riiatw/",
+                redirect: false,
+                icon: "language"
+            },
+            {
+                name: "Gallery",
+                path: "/gallery/",
+                redirect: false,
+                icon: "photo_library"
+            }
+        ]
+    },
+    {
+        name: "tools & stuff",
+        links: [
+            {
+                name: "\"AI\" Chat",
+                path: "/chat/",
+                redirect: false,
+                icon: "smart_toy"
+            },
+            {
+                name: "Split It!",
+                path: "/utils/split-it/",
+                redirect: false,
+                icon: "space_dashboard"
+            },
+            {
+                name: "Nekobox",
+                path: "/utils/nekobox/",
+                redirect: false,
+                icon: "inventory_2"
+            },
+            {
+                name: "The Inaccessible Website (soon)",
+                path: "https://inaccessible.nekoweb.org",
+                redirect: true,
+                icon: "not_accessible"
+            },
+            {
+                name: "jPlayer2",
+                path: "https://jbcarreon123.github.io/jPlayer2",
+                redirect: true,
+                icon: "music_note"
+            },
+            {
+                name: "nkko.link",
+                path: "https://nkko.link",
+                redirect: true,
+                icon: "link"
+            },
+            {
+                name: "Nekoweb Deployment Adapters",
+                path: "https://deploy.nekoweb.org",
+                redirect: true,
+                icon: "rocket_launch"
+            },
+            {
+                name: "simple-webring-redirect",
+                path: "https://github.com/jbcarreon123/simple-webring-redirect",
+                redirect: true,
+                icon: "call_split"
+            }
+        ]
+    },
+    {
+        name: "writing",
+        links: [
+            {
+                name: "Posts",
+                path: "/posts/",
+                redirect: false,
+                icon: "article"
+            },
+            {
+                name: "Tutorials",
+                path: "/tutorials/",
+                redirect: false,
+                icon: "help"
+            }
+        ]
+    },
+    {
+        name: "my webrings",
+        links: [
+            {
+                name: "SSGRing",
+                path: "/webrings/ssgring/",
+                redirect: false,
+                icon: "build"
+            },
+            {
+                name: "Responeko",
+                path: "/webrings/responeko/",
+                redirect: false,
+                icon: "phone_android"
+            },
+            {
+                name: "YAMring",
+                path: "/webrings/yamring/",
+                redirect: false,
+                icon: "tag"
+            }
+        ]
+    },
+    {
+        name: "outlinks",
+        links: [
+            {
+                name: "Links",
+                path: "/links/",
+                redirect: false,
+                icon: "link"
+            },
+            {
+                name: "Webrings",
+                path: "/webrings/",
+                redirect: false,
+                icon: "donut_large"
+            },
+            {
+                name: "Bookmarks",
+                path: "/bookmarks/",
+                redirect: false,
+                icon: "bookmark"
+            }
+        ]
+    }
+]
