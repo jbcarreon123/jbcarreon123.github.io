a9ed8e9dafcee88e88ff43fb1e1c69546ae99d0c
Jb Carreon <jbcarreon212@gmail.com>
2025-06-15 22:07:17 +0800

    site prv and stuff

--- src/components/Nekocafe.svelte
+++ src/components/Nekocafe.svelte
@@ -1,9 +1,33 @@
 <script>
-	import { onMount } from 'svelte';
+	import { onMount } from "svelte";
 
-	const username = 'jbcarreon123'; // change the username!!!
-	const posts_url = 'https://cafe.frizzbees.dev/get_posts/1?name=';
-	const post_url = 'https://social.nekoweb.org/post/?id=';
+	const username = "jbcarreon123"; // change the username!!!
+	const posts_url = "https://cafe.frizzbees.dev/get_posts/1?name=";
+	const post_url = "https://social.nekoweb.org/post/?id=";
+
+	function timeAgo(timestamp) {
+		const now = new Date().getTime();
+		const diffInSeconds = Math.floor((now - timestamp) / 1000);
+
+		if (diffInSeconds < 60) {
+			return "just now";
+		} else if (diffInSeconds < 3600) {
+			const minutes = Math.floor(diffInSeconds / 60);
+			return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
+		} else if (diffInSeconds < 86400) {
+			const hours = Math.floor(diffInSeconds / 3600);
+			return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+		} else if (diffInSeconds < 2592000) {
+			const days = Math.floor(diffInSeconds / 86400);
+			return `${days} day${days > 1 ? "s" : ""} ago`;
+		} else if (diffInSeconds < 31536000) {
+			const months = Math.floor(diffInSeconds / 2592000);
+			return `${months} month${months > 1 ? "s" : ""} ago`;
+		} else {
+			const years = Math.floor(diffInSeconds / 31536000);
+			return `${years} year${years > 1 ? "s" : ""} ago`;
+		}
+	}
 
 	// thanks max
 	async function loadStatus() {
@@ -11,24 +35,32 @@
 		let json = await request.json();
 		json = json[0];
 
-		let timestamp = json['timestamp'] * 1000;
-		let time = new Date(timestamp).toUTCString();
+		let timestamp = json["timestamp"] * 1000;
 
 		return {
 			name: json["name"],
 			post: json["post"],
 			id: post_url + json["id"],
-			time: time
-		}
-	};
+			time: timeAgo(timestamp),
+		};
+	}
 </script>
 
 <div>
 	{#await loadStatus()}
 		<p>Loading rambles...</p>
 	{:then out}
-		<a href={out.id}><p>{out.post}</p></a>
+		<a href={out.id} class="sta tg">
+			{out.time}
+		</a>
+		<p>{out.post}</p>
 	{:catch err}
 		<p>Error occured. {err}</p>
 	{/await}
+
+	<style>
+        a.sta.tg {
+            padding-top: 0 !important;
+        }
+    </style>
 </div>
